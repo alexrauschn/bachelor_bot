93c61a494dc0b7229a17a9810461e47ee226f5dc
faelb <faelb@gmx.at>
2021-05-15 11:48:48 +0200

    post done in flask

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/question', methods=['POST'])
+def getAnswer():
+    question = request.form['question']#not sure, maybe this will be a json later on but its not important now
+
+#TODO now implement the code that uses the question and return it later on
+
+    answer = "Here you have to put the answer string!"
+    return jsonify({'most fitting answer ': answer})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
