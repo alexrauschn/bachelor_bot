aea837ef12aa8dad09326fb64d58205d1ec67947
Vince <schauervincent@gmail.com>
2021-05-15 16:59:14 +0200

    basic workflow works

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, jsonify, request
-
+from model import classify, response
 app = Flask(__name__)
-
 
 @app.route('/question', methods=['POST'])
 def getAnswer():
-    question = request.form['question']#not sure, maybe this will be a json later on but its not important now
+    question = request.get_json()['question']#not sure, maybe this will be a json later on but its not important now
 
 #TODO now implement the code that uses the question and return it later on
-
-    answer = "Here you have to put the answer string!"
-    return jsonify({'most fitting answer ': answer})
+    print(question)
+    answer = response(question)
+    if answer == "":
+        answer = "Sorry no idea what you're talking about, bruh"
+    return jsonify({'most fitting answer ' : answer})
 
 
 if __name__ == '__main__':
