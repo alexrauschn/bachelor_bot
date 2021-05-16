702ddc3772fe47068bcd04ad4cd2de78c18ba4e7
alexrauschn <gh800ub3Br3eiAMb>
2021-05-16 16:46:12 +0200

    gesetzescomparer funzt

--- gesetzcomparer.py
+++ gesetzcomparer.py
@@ -1,19 +1,7 @@
 import requests
-import re
+# import re
+import filecmp
 from bs4 import BeautifulSoup
-
-
-def write_gastbewerbetext_to_file(filename, url):
-    start = '<h2 class="UeberschrPara AlignCenter">Gastgewerbe</h2>'
-    end = '<h2'
-
-    s = requests.get(url).text
-    #only get relevant part of text
-    gastgewerbe_absatz = BeautifulSoup(((s.split(start))[1].split(end)[0]), 'lxml')
-
-    f = open(filename, "w+", encoding="utf-8")
-    f.write(gastgewerbe_absatz.text)
-    f.close()
 
 
 # 1. local txt laden
@@ -21,17 +9,40 @@ def write_gastbewerbetext_to_file(filename, url):
 # 3. comparen
 # 4. rückgabe 1 oder 0
 
+# get the live file and write a local copy of the plain text
+def write_gastbewerbetext_to_file(filename, url):
+    start = '<h2 class="UeberschrPara AlignCenter">Gastgewerbe</h2>'
+    end = '<h2'
 
+    # get content of passed path / url
+    if "http" in url:
+        s = requests.get(url).text
+    else:
+        temp_s = open(url, "r", encoding="utf-8")
+        s = temp_s.read()
+        temp_s.close()
+
+    # only get relevant part of text, including html tags
+    gastgewerbe_absatz = BeautifulSoup(((s.split(start))[1].split(end)[0]), 'lxml')
+
+    # write cropped part into file
+    f = open(filename, "w+", encoding="utf-8")
+
+    # only get the text, discard html tags
+    f.write(gastgewerbe_absatz.text)
+    f.close()
+
+# get the current URL of the live file
 def get_current_url():
+    # get link area
     start = '<h2 id="-span-aktuelles-span-span-span-"><span>Aktuelles: </span><span></span></h2>'
     end = '<ul>'
     original_url = 'https://www.sozialministerium.at/Informationen-zum-Coronavirus/Coronavirus---Rechtliches.html'
     s = requests.get(original_url).text
     # print(s)
     long_string = ((s.split(start))[1].split(end)[0])
-
-   # start = 'Ausgegeben am '
-   # end = '</p>'
+    #start = 'Ausgegeben am '
+    #end = '</p>'
     #date_original = ((s.split(start))[1].split(end)[0])
 
     # get link itself
@@ -41,16 +52,21 @@ def get_current_url():
     return short_string + ".html"
 
 def compare_files(oldFilename, newFilename):
-    oldfFile = open(oldFilename, "r", encoding="utf-8")
-    newFile = open(newFilename, "r", encoding="utf-8")
-    return oldfFile.read() == newFile.read()
+    check = filecmp.cmp(oldFilename, newFilename)
+    return check
+
+#   oldFile = open(oldFilename, "r", encoding="utf-8")
+#   newFile = open(newFilename, "r", encoding="utf-8")
+#   return oldFile.read() == newFile.read()
+
+
 
 def are_local_files_up_to_date():
-    newGewerbeTextFilename = "newText.txt"
-    oldGewerbeTextFileName = "currentText.txt"
-    oldGewerbeTexthtml = "currentText.html"
+    newGewerbeTextFilename = "liveText.txt"
+    oldGewerbeTextFileName = "localText.txt"
+    oldGewerbeTexthtml = "local.html"
     write_gastbewerbetext_to_file(newGewerbeTextFilename, get_current_url())
-    write_gastbewerbetext_to_file(oldGewerbeTextFileName, get_current_url())
+    write_gastbewerbetext_to_file(oldGewerbeTextFileName, oldGewerbeTexthtml)
 
     if(compare_files(oldGewerbeTextFileName, newGewerbeTextFilename)):
         print("same")
@@ -58,5 +74,4 @@ def are_local_files_up_to_date():
         print("they're different")
 
 are_local_files_up_to_date()
-#are_local_files_up_to_date()
 
