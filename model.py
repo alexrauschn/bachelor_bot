910743749c8b6167251b86bd8bba0d26a736ac50
alexrauschn <gh800ub3Br3eiAMb>
2021-05-30 19:25:16 +0200

    millions of comments and ideas

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ stemmer = LancasterStemmer()
 # things we need for Tensorflow
 import numpy as np
 import tflearn
+# tensorflow can be imported via the code from line 10 or from line 11; use the one that works on your machine
 #import tensorflow as tf
 from tensorflow.python.framework import ops as tf
 import random
@@ -19,21 +20,32 @@ with open('intents.json') as json_data:
 words = []
 classes = []
 documents = []
+# set which words to ignore and not count as matches
 ignore_words = ['?']
-# loop through each sentence in our intents patterns
+# loop through each sentence in our intents patterns until we are through
+# patterns are  (sample) sentences / questions / keywords and intents are categories (each item in one intent
+# shares a tag and belongs to the same classification
+# "intent" and "intents" seem to be "known" key names; ['intents'] however is the NAME of a list (interpreted as such), as is ['patterns']
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         # tokenize each word in the sentence
+        # information on tokenization: https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/text/Tokenizer
+        # more: https://chatbotsmagazine.com/contextual-chat-bots-with-tensorflow-4391749d0077
+        # "We create a list of documents (sentences), each sentence is a list of stemmed words and each document is
+        # associated with an intent (a class). (...) we need to transform it further: from documents of words into tensors of numbers."
         w = nltk.word_tokenize(pattern)
         # add to our words list
         words.extend(w)
         # add to documents in our corpus
         documents.append((w, intent['tag']))
-        # add to our classes list
+        # add to our classes list (unless it already exists)
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
 # stem and lower each word and remove duplicates
+# about stemming: https://www.h2kinfosys.com/blog/stemming-and-lemmatization/
+# "Stemming is a text normalizing technique that cuts down affixes of words, to extract its base form or root words.
+# Stemming is a crude process and sometimes, the root word, also called the stem, may not have grammatical meaning."
 words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
 
@@ -86,7 +98,7 @@ net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
 net = tflearn.regression(net)
 
-# Define model and setup tensorboard
+# Define model and set up tensorboard
 model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
 # Start training (apply gradient descent algorithm)
 model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
@@ -114,6 +126,7 @@ def bow(sentence, words, show_details=False):
 
     return(np.array(bag))
 
+# manually pass a sentence to be categorized HERE; otherwise handled via frontend
 p = bow("is your shop open today?", words)
 print (p)
 print (classes)
@@ -124,6 +137,7 @@ print(model.predict([p]))
 pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
 
 
+# set minimum threshold for viable categorization
 ERROR_THRESHOLD = 0.25
 def classify(sentence):
     # generate probabilities from the model
@@ -147,21 +161,118 @@ def response(sentence, userID='123', show_details=False):
             for i in intents['intents']:
                 # find a tag matching the first result
                 if i['tag'] == results[0][0]:
-                    # a random response from the intent
+                    # a random response from the intent; only relevant if multiple responses exist
                     return random.choice(i['responses']), i['link']
 
             results.pop(0)
 
 # to do:
+
 # tokenize items as is
+# read: https://de.slideshare.net/ahmadhussein45/expert-system-with-python-2
+
 # use NLTK to process input
+# => Good URL on how to use NLTK to process strings
+# => https://stackoverflow.com/questions/8897593/how-to-compute-the-similarity-between-two-text-documents
+
 # pass this pre-processed input to a quantification function
+
 # count matches between input and patterns
+# => potentially just use a dict instead of tensorflow? see https://stackoverflow.com/questions/47992091/how-to-match-the-keywords-in-paragraph-using-python-nltk/47992508
+# => tutorial on how to use the respective indicators:
+# => https://bommaritollc.com/2014/06/30/advanced-approximate-sentence-matching-python/
+
 # find good algorithm: percentage of patterns matched? total count?
-# add points for respective tag whenever it matches
+# => when counting matches by hand, either only count unique matches OR weigh matches with longer strings more
+# example: query is "kann ich mit 4 freunden draußen sitzen?", and the tag "outdoor" has that exact question. that would yield only ONE point otherwise
+# it might even come so that "freunde" and "sitzen" are individual items for the tag "indoors" - yielding two points and associating the query with
+# that category! so either for matches, only tokenize unique matches (full phrases match the longest match, not single words from it as well)
+# or weigh them by length. or both! experiment with what works best...
+# maybe: transform everything into lists; then just get unique list entries, then just compare list contents
+# convert array to lists: https://www.journaldev.com/32797/python-convert-numpy-array-to-list
+# convert everything to lowercase: https://stackoverflow.com/questions/1801668/convert-a-python-list-with-strings-all-to-lowercase-or-uppercase
+# unique lists: https://www.geeksforgeeks.org/python-get-unique-values-list/
+# sort lists alphabetically: https://www.kite.com/python/answers/how-to-sort-a-list-alphabetically-in-python
+# compare lists: https://www.geeksforgeeks.org/python-percentage-similarity-of-lists/
+
+# !!!
+
+# STEP 1: tokenize the INTENTS via NLTK
+# this already happened. the resulting list without duplicates is "classes"
+# STEP 2: tokenize the INPUT via NLTK
+# STEP 3: create multi dimensional lists
+# STEP 4: compare these lists
+# STEP 5: save the matching scores
+# STEP 6: rank the matching scores
+
+
+# easier via NLTK: https://stackoverflow.com/questions/49939450/unique-word-frequency-using-nltk
+# TOKENIZE of the NLTK to split sentences into single words
+# https://dev.to/coderasha/compare-documents-similarity-using-python-nlp-4odp
+# now, only leave unique words: https://www.tutorialspoint.com/python_text_processing/python_filter_duplicate_words.htm
+# some ideas: https://stackoverflow.com/questions/19648959/lists-in-python-using-nltk
+# google some more...
+# how to compare these? where do the tokenized words go? what is the format?
+# => convert to a LIST (idea: with THREE dimensions)
+# automatically NUMBER every (intent) list generated
+# some ideas: https://stackoverflow.com/questions/12203652/in-python-create-a-list-with-a-variable-in-the-name
+# https://stackoverflow.com/questions/48598222/appending-items-to-a-list-and-ensuring-unique-name-by-incrementing-number
+# possible do this via a list of lists, yes (with answers at position 3 (= 2) in the rows (= [2][0])
+# append each new list to the list of lists, the superlist contains all the lists
+# the sublists contain the tag (= category) in row 1, the key words in row 2, and the reply in row 3
+# compare list [n][1] to the input after tokenization => GOOGLE HOW TO DO THAT
+# SAVE the resulting match percentage in the list of lists; it contains to values: the position of the list and the match
+# the match percentage is then compared; the one with the highest "wins" and its corresponding position number
+# is used to go to the respective list and get the reply from position ([0][n])[2][0]
+# find highest value of a list: https://www.tutorialspoint.com/python/list_max.htm
+# could just save the position of that in a respective temp variable then, use this to
+# what if two values are the same?
+# percentage-wise match? would punish many key words... (a list with just one generic word would always win)
+# absolute match? punish few key words... (a list with tons of words would always win)
+# WEIGH BOTH THE SAME
+# better idea:
+# go through every category and count the matching words (absolute number). save that number as "number 1"
+# now divide that number by the amount of items in each category - this gives the percentage wise match. save that number as "number 2".
+# compare every number 1 to one another. the category with the highest score is in place 1, the category with the second most matches is place 2...
+# save that resulting placement as "placement 1" for every category
+# now just compare the scoring with regards to "number 2". again reward placements for every category and save them as "placement 2"
+# for every category, add the value of placement 1 + value of placement 2 into the new variable "placement 3", then divide this by two
+# this is a ranking that weighs both counting methods equally
+# return the reply of the category with the lowest placement 3 score
+
+# get number of elements in list: https://stackabuse.com/python-get-number-of-elements-in-a-list/
+# get number of matching elements between two lists: https://www.geeksforgeeks.org/python-count-of-matching-elements-among-lists-including-duplicates/
+# divide number of matches by the number of words in tag-list. this varies between 0 and 1 (nothing and everything)
+# total number of matches and percentage wise match should both count for 50%
+# both should have the same RANGE, but ACROSS categories
+# alle wörter zählen, durch anzahl kategorien teilen, das als divisor
+
+# sort the LIST
+# compare the LISTS (input is also to become such a prepared list!)
+# compare the different lists
+
+# google search for "python categorize input word keywords"
+# => https://stackoverflow.com/questions/62883961/how-to-categorize-a-list-of-data-by-keyword-in-python
+# => https://stackoverflow.com/questions/1490061/classifying-text-based-on-groups-of-keywords
+#
+# -use 3 dimensional lists: dimension 1 (category), dimension 2 (loooots of key words), dimension 3 (reply) => give item [2][0] as reply, use category [0][0] as category, and [1][n] for key words (while in...)
+# -convert every single word into a list item of its own
+# -ignore puntucation and ()"' and so on
+# -convert the list to lowercase
+# -filter out duplicates
+# -order the list alphabetically
+# -do the same for the input
+# -match lists percentage-wise
+# -weigh longer strings for higher percentage
+# -algorithm: number of characters of each matching word gets counted
+
+# add points for respective tag whenever it matches (see line above - find a good way to quantify a good fit)
+
 # at the end: list categories with most points
-# optional: point threshold?
+# => optional: point threshold?
+
 # give list with best matches
+# => compare this to the results of the DL algorithm
 
 print("\n----------\n")
 print(classify('is your shop open today?'))
---
+++ percentageMatch.py
@@ -0,0 +1,116 @@
+# things we need for NLP
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+from model import words, classes, ignore_words
+from model import clean_up_sentence
+
+# import our chat-bot intents file
+import json
+with open('intents.json') as json_data:
+    intents = json.load(json_data)
+
+# define variables
+xp_words = words
+question_word = []
+xp_classes = classes
+xp_documents = []
+
+question = "kann ich mit freunden draussen sitzen?"
+
+# function to tokenize the question
+def tokenizeInput(question):
+    global question_word
+    w = nltk.word_tokenize(question)
+    # add w to our "simple" words list; this is the question in the form of a list
+    question_word.extend(w)
+    # stem question words
+    question_word = [stemmer.stem(w.lower()) for w in question_word if w not in ignore_words]
+    question_word = sorted(list(set(question_word)))
+    # remove duplicates
+    # question_classes = sorted(list(set(question_classes)))
+
+# or just use...
+# question_classes = clean_up_sentence(question)
+
+# or: adapt previous class (from model.py) to create the desired array / list structure, starting line 28
+# is ['intents'] just a row name? :o
+
+# convert results into a list of lists. the super list contains the lists, the sublists have the following dimensions:
+# [n][0][#]: key words
+# [n][1][0]: response
+# [n][2][0,1]: absolute and relative score
+# [n][3][0,1,2]: absolute, relative and combined rank
+
+
+
+# sort lists so they contain the tag, keywords and response in one row respectively...
+# response class uses a LIST, structure can be inferred by looking at that class...
+
+# compare the lists
+# generate two sets of rankings
+
+# more sophisticated approach:
+# https://stackoverflow.com/questions/8897593/how-to-compute-the-similarity-between-two-text-documents
+
+
+# step 1: tokenize and stem json word lists
+#   => this is already done; however, the format is not exactly what we need
+# step 2: convert this into a list of lists
+# okay, maybe...
+# HIER MUSS MAN DAS IRGENDWIE SO HINKRIEGEN DASS ER IN JEDER ZEILE EINEN NEUEN TAG HAT
+# also: erste spalte die schlüsselwörter, zweite spalte die antwort. ersteres eine liste, zweiteres ein string
+# dritte spalte dann anzahl übereinstimmungen absolut, vierte spalte relativ, fünfte rang absolut, sechste rang relativ, siebte rang gemischt
+# neue zeile = neues intent
+xp_list = []
+
+
+# https://blog.finxter.com/wp-content/uploads/2020/04/listoflist-768x432.jpg
+# liste 1: 7 einträge
+# spalte 1: list of lists (als inhalt)
+# list of lists hat: wortliste 1, wortliste 2, wortliste 3...
+# die wortlisten haben jeweils variable längen
+# spalten 2 - 7: arrays mit jeweils einem eintrag pro spalte (item 1, 2, 3...)
+# eintrag NUMMER eins der spalte gehört zu wortliste 1, eintrag nummer zwei zu wortliste zwei...
+
+# hier machen die das mit dictionaries: https://online.datasciencedojo.com/blogs/building-a-rule-based-chatbot-in-python
+# cosine match: https://www.geeksforgeeks.org/python-measure-similarity-between-two-sentences-using-cosine-similarity/
+
+xp_liste = [[]],[],[],[],[],[],[]
+append to xp_liste[0]
+# => neue liste dranhängen? geht das mit append? dass er erkennt welcher elementtyp (bei position 0: liste) erwartet wird?
+# append oder insert into?
+# jedenfalls dann auch bei position 2 - 7 mit append? dass er jeweils ein neues item generieren würde??
+
+# siehe https://blog.finxter.com/python-list-of-lists/
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        w = nltk.word_tokenize(pattern)
+        # add to our words list
+        xp_words.extend(w)
+
+        # mein codeversuch...
+        xp_list.insert(0, [w])
+
+        # add to documents in our corpus
+        xp_documents.append((w, intent['tag']))
+        # add to our classes list (unless it already exists)
+        if intent['tag'] not in xp_classes:
+            xp_classes.append(intent['tag'])
+
+# stem and lower each word and remove duplicates
+xp_words = [stemmer.stem(w.lower()) for w in xp_words if w not in ignore_words]
+xp_words = sorted(list(set(words)))
+
+# xp_words = [][[]][]
+# xp_words[n][0] = intent['tag']
+# xp_words[n][1] = [tagliste]
+# xp_words[n][2] = intent['reply']
+
+# step 3: tokenize and stem the input question
+# step 4: convert this into a list
+# step 5: compare the sublists with the question list
+# step 6: quantify the match
+# step 7: return the N for the best match
+# step 8: return the replied tied to this N
